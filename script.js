1745701d027fe51ec337037976ee89bbb50c205a
Kamocca <86765533+Kamocca1@users.noreply.github.com>
2025-07-18 17:11:07 +0200

    feat(core): create Cell and Gameboard modules
    Implemented Cell and Gameboard modules to manage the game state for Tic Tac Toe.
    The Cell module handles individual cell values, while the Gameboard module manages the board structure and token placement.

--- script.js
+++ script.js
@@ -1,4 +1,59 @@
-const Gameboard = (function () {
-const gameboard = [];
-return {}
+const Cell = (function () {
+    let value = 0;
+
+    const addToken = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        addToken,
+        getValue,
+    };
 })();
+
+const Gameboard = (function () {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i][j] = Cell();
+        }
+    }
+
+    const getBoard = () => board;
+
+    const dropToken = (column, player) => {
+        const availableCells = board
+            .filter((row) => row[column].getValue() === 0)
+            .map((row) => row[column]);
+
+        if (!availableCells.length) return;
+
+        const lowestRow = availableCells.length - 1;
+        board[lowestRow][column].addToken(player);
+    };
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => {
+            row.map((cell) => {
+                cell.getValue();
+            });
+        });
+        console.log(boardWithCellValues);
+    };
+
+    return {
+        getBoard,
+        dropToken,
+        printBoard,
+    };
+})();
+
+function Player() {}
+
+const GameController = (function () {})();
